3da02afd8a3382f707f500979ab91d2c118570c6
Sergi <sergikaralashvili@gmail.com>
2023-02-16 23:46:29 +0400

    updated functions

--- cooperate.js
+++ cooperate.js
@@ -1,14 +1,32 @@
-const numberA = document.querySelector("input-a");
-const numberB = document.querySelector("input-b");
-const button = document.querySelector("button");
-const goodResult = document.querySelector("result-out");
-const selector = document.querySelector("selector");
+const numberA = document.querySelector(".input-a");
+const numberB = document.querySelector(".input-b");
+let button = document.querySelector(".button");
+let goodResult = document.querySelector(".result-out");
+let selector = document.querySelector(".selector");
+let contentA = 0;
+let contentB = 0;
+numberA.addEventListener("input", function () {
+  contentA = numberA.value;
+});
+numberB.addEventListener("input", function () {
+  contentB = numberB.value;
+});
+
+function change() {
+  if (numberA !== null) {
+    return numberA.value;
+  } else if (numberB !== null) {
+    return numberB.value;
+  } else {
+    return "Error";
+  }
+}
 
 button.addEventListener("click", function () {
-  const a = Number(numberA.value);
-  const b = Number(numberB.value);
-  const operation = selector.value;
-  const result = calculator({ a, b, operation });
-
+  let a = Number(numberA);
+  let b = Number(numberB);
+  let operation = selector.value;
+  let result = calculator({ a, b, operation });
+  console.log(a, b, operation);
   selector.innerHTML = result;
 });
